b79033125df8f415f4a24b8aca7d5207a0c640f0
Chhean Silapin <slpgaming23@gmail.com>
2025-02-17 19:55:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include './includes/head.php'; ?>
+
+<body class="animsition">
+
+    <!-- Header -->
+    <?php include './includes/header.php'; ?>
+
+    <!-- Cart -->
+
+    <?php include './includes/cart.php'; ?>
+
+    <!-- Slider -->
+
+
+
+    <!-- Banner -->
+
+
+
+    <!-- Product -->
+
+
+
+    <!-- Footer -->
+
+<?php include './includes/footer.php'; ?>
+
+    <!-- Back to top -->
+
+<?php include './includes/btntop.php'; ?>
+    <!-- Modal1 -->
+
+
+<?php include './includes/modal.php'; ?>
+
+    <?php include './includes/foot.php'; ?>
+
+</body>
+
+</html>
