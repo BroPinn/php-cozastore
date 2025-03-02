5306c921a870065405dd2308ee3bd8289db85c12
BroPinn <slpgaming23@gmail.com>
2025-03-03 00:08:12 +0700

    work at admin

--- database.php
+++ database.php
@@ -1,6 +1,4 @@
 <?php
-// c:\xampp\htdocs\m3\cozastore-master\database.php
-
 function connectToDatabase() {
     $host = 'localhost';
     $dbname = 'onestore_db';
@@ -8,12 +6,15 @@ function connectToDatabase() {
     $password = '';
 
     try {
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
         $pdo = new PDO($dsn, $username, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false // Improves security for prepared statements
         ]);
+
+        // Set the collation
+        $pdo->exec("SET NAMES 'utf8mb4' COLLATE 'utf8mb4_general_ci'");
 
         return $pdo;
     } catch (PDOException $e) {
@@ -21,4 +22,3 @@ function connectToDatabase() {
         return null; // Return null instead of throwing an exception
     }
 }
-
