8a5c641809862c3aa5a027d1e34e553c42ecf9ed
unknown <Lydavidho@gmail.com>
2025-03-21 08:25:03 +0700

    add to cart

--- views/checkout.view.php
+++ views/checkout.view.php
@@ -13,9 +13,8 @@
     </div>
 </div>
 
-
 <!-- Shoping Cart -->
-<form class="bg0 p-t-75 p-b-85">
+<form class="bg0 p-t-75 p-b-85" id="checkoutForm" action="PayWayApiCheckout.php" method="POST">
     <div class="container">
         <div class="row">
             <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
@@ -29,90 +28,68 @@
                                 <th class="column-4">Quantity</th>
                                 <th class="column-5">Total</th>
                             </tr>
-                            <tr id="cartItem">
-
+                            <tr class="table_row" data-price="36.00">
+                                <td class="column-1">
+                                    <div class="how-itemcart1">
+                                        <img src="./assets/images/item-cart-04.jpg" alt="IMG">
+                                    </div>
+                                </td>
+                                <td class="column-2">Fresh Strawberries</td>
+                                <td class="column-3">$ <span class="item-price">36.00</span></td>
+                                <td class="column-4">
+                                    <div class="wrap-num-product flex-w m-l-auto m-r-0">
+                                        <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+                                            <i class="fs-16 zmdi zmdi-minus"></i>
+                                        </div>
+                                        <input class="mtext-104 cl3 txt-center num-product" type="number"
+                                            name="num-product1" value="1">
+                                        <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+                                            <i class="fs-16 zmdi zmdi-plus"></i>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="column-5">$ <span class="item-total">36.00</span></td>
                             </tr>
-                            <!-- <tr class="table_row"> 
-								<td class="column-1">
-									<div class="how-itemcart1">
-										<img src="./assets/images/item-cart-04.jpg" alt="IMG">
-									</div>
-								</td>
-								<td class="column-2">Fresh Strawberries</td>
-								<td class="column-3">$ 36.00</td>
-								<td class="column-4">
-									<div class="wrap-num-product flex-w m-l-auto m-r-0">
-										<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
-											<i class="fs-16 zmdi zmdi-minus"></i>
-										</div>
-
-										<input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product1" value="1">
-
-										<div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
-											<i class="fs-16 zmdi zmdi-plus"></i>
-										</div>
-									</div>
-								</td>
-								<td class="column-5">$ 36.00</td>
-							</tr> -->
-
-
                         </table>
                     </div>
 
                     <div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
                         <div class="flex-w flex-m m-r-20 m-tb-5">
-                            <input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Coupon Code">
-
-                            <div class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
-                                Apply coupon
-                            </div>
+                            <input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text"
+                                name="coupon" placeholder="Coupon Code">
+                            <div
+                                class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
+                                Apply coupon</div>
                         </div>
-
-                        <div class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
-                            Update Cart
-                        </div>
+                        <div
+                            class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
+                            Update Cart</div>
                     </div>
                 </div>
             </div>
 
             <div class="col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50">
                 <div class="bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm">
-                    <h4 class="mtext-109 cl2 p-b-30">
-                        Cart Totals
-                    </h4>
-
+                    <h4 class="mtext-109 cl2 p-b-30">Cart Totals</h4>
                     <div class="flex-w flex-t bor12 p-b-13">
                         <div class="size-208">
-                            <span class="stext-110 cl2">
-                                Subtotal:
-                            </span>
+                            <span class="stext-110 cl2">Subtotal:</span>
                         </div>
-
                         <div class="size-209">
-                            <span class="mtext-110 cl2">
-                                $0
-                            </span>
+                            <span class="mtext-110 cl2" id="cartSubtotal">$36.00</span>
                         </div>
                     </div>
-
                     <div class="flex-w flex-t bor12 p-t-15 p-b-30">
                         <div class="size-208 w-full-ssm">
-                            <span class="stext-110 cl2">
-                                Shipping:
-                            </span>
+                            <span class="stext-110 cl2">Shipping:</span>
                         </div>
-
                         <div class="size-209 p-r-18 p-r-0-sm w-full-ssm">
                             <p class="stext-111 cl6 p-t-2">
-                                There are no shipping methods available. Please double check your address, or contact us if you need any help.
+                                There are no shipping methods available. Please double check your address, or contact us
+                                if you need any help.
                             </p>
-
                             <div class="p-t-15">
-                                <span class="stext-112 cl8">
-                                    Calculate Shipping
-                                </span>
-
+                                <span class="stext-112 cl8">Calculate Shipping</span>
                                 <div class="rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9">
                                     <select class="js-select2" name="time">
                                         <option>Select a country...</option>
@@ -121,42 +98,33 @@
                                     </select>
                                     <div class="dropDownSelect2"></div>
                                 </div>
-
                                 <div class="bor8 bg0 m-b-12">
-                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="state" placeholder="State /  country">
+                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="state"
+                                        placeholder="State /  country">
                                 </div>
-
                                 <div class="bor8 bg0 m-b-22">
-                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="postcode" placeholder="Postcode / Zip">
+                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="postcode"
+                                        placeholder="Postcode / Zip">
                                 </div>
-
                                 <div class="flex-w">
-                                    <div class="flex-c-m stext-101 cl2 size-115 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer">
-                                        Update Totals
-                                    </div>
+                                    <div
+                                        class="flex-c-m stext-101 cl2 size-115 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer">
+                                        Update Totals</div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-
                     <div class="flex-w flex-t p-t-27 p-b-33">
                         <div class="size-208">
-                            <span class="mtext-101 cl2">
-                                Total:
-                            </span>
+                            <span class="mtext-101 cl2">Total:</span>
                         </div>
-
                         <div class="size-209 p-t-1">
-                            <span class="mtext-110 cl2">
-                                $0
-                            </span>
+                            <span class="mtext-110 cl2" id="cartTotal">$36.00</span>
                         </div>
                     </div>
-
-                    <button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
-                        Proceed to Checkout
-                    </button>
+                    <button type="button"
+                        class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">Proceed to
+                        Checkout</button>
                 </div>
             </div>
         </div>
@@ -166,3 +134,45 @@
 <?php include './includes/btntop.php'; ?>
 <?php include './includes/footer.php'; ?>
 <?php include './includes/foot.php'; ?>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const calculateTotals = () => {
+        let subtotal = 0;
+        document.querySelectorAll('.table_row').forEach(row => {
+            const price = parseFloat(row.getAttribute('data-price'));
+            const quantity = parseInt(row.querySelector('.num-product').value);
+            const total = price * quantity;
+            row.querySelector('.item-total').textContent = total.toFixed(2);
+            subtotal += total;
+        });
+        document.getElementById('cartSubtotal').textContent = `$${subtotal.toFixed(2)}`;
+        document.getElementById('cartTotal').textContent = `$${subtotal.toFixed(2)}`;
+    };
+
+    document.querySelectorAll('.btn-num-product-down').forEach(button => {
+        button.addEventListener('click', function(e) {
+            const input = e.target.closest('.wrap-num-product').querySelector('.num-product');
+            let value = parseInt(input.value);
+            if (value > 1) {
+                value--;
+                input.value = value;
+                calculateTotals();
+            }
+        });
+    });
+
+    document.querySelectorAll('.btn-num-product-up').forEach(button => {
+        button.addEventListener('click', function(e) {
+            const input = e.target.closest('.wrap-num-product').querySelector('.num-product');
+            let value = parseInt(input.value);
+            value++;
+            input.value = value;
+            calculateTotals();
+        });
+    });
+
+    // Initial calculation
+    calculateTotals();
+});
+</script>
